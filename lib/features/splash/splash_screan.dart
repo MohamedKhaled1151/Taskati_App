c52a6859df91c3fa25384000917b08f83a6c7b37
MohamedKhaled1151 <khaledeldgwy2040@icloud.com>
2025-07-09 20:15:49 +0300

    system design

---
+++ lib/features/splash/splash_screan.dart
@@ -0,0 +1,12 @@
+
+
+   import 'package:flutter/material.dart';
+
+class SplashScrean extends StatelessWidget {
+     const SplashScrean({super.key});
+
+     @override
+     Widget build(BuildContext context) {
+       return Scaffold();
+     }
+   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 
-void main (){
+import 'package:flutter/cupertino.dart';
+import 'package:taskati/taskati_app.dart';
 
+void main (){
+ runApp(TaskatiApp());
 }
 

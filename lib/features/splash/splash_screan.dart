1a895ef909359dd452c7cd60c82d4dbec50012e1
MohamedKhaled1151 <khaledeldgwy2040@icloud.com>
2025-07-11 15:11:55 +0300

    installing screenutil

--- lib/features/splash/splash_screan.dart
+++ lib/features/splash/splash_screan.dart
@@ -1,12 +1,33 @@
 
 
    import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScrean extends StatelessWidget {
      const SplashScrean({super.key});
 
      @override
      Widget build(BuildContext context) {
-       return Scaffold();
+       return Scaffold(
+         body: Center(
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Image.asset("assets/images/Animation - 1751837012840.gif"),
+               SizedBox(height:20.h),
+               Text("Taskati",
+                 style: TextStyle
+                   (fontSize: 20.sp,fontWeight: FontWeight.bold,),),
+               SizedBox(height:15.h),
+               Text("Manage your daily tasks easily and stay productive with Taskati",
+                 style: TextStyle
+                   (fontSize: 10.sp,fontWeight: FontWeight.bold,
+                 color:Colors.black54),)
+
+
+             ],
+           ),
+         ) ,
+       );
      }
    }
--- lib/taskati_app.dart
+++ lib/taskati_app.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:taskati/features/splash/splash_screan.dart';
 
 class TaskatiApp extends StatelessWidget {
@@ -8,9 +9,14 @@ class TaskatiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScrean(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScrean(),
+      ),
     );
   }
 }

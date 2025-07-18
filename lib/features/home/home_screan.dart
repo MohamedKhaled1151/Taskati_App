305fd2e19e4b550de70ce680b2e36ecab1666aad
mohamed <https://github.com/MohamedKhaled1151>
2025-07-18 09:47:18 +0300

    Home Screan

--- lib/features/home/home_screan.dart
+++ lib/features/home/home_screan.dart
@@ -8,14 +8,15 @@ class  HomeScrean extends StatefulWidget {
   @override
   State< HomeScrean> createState() => _State();
 
-
 }
 
 class _State extends State< HomeScrean> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
 
--- lib/features/splash/splash_screan.dart
+++ lib/features/splash/splash_screan.dart
@@ -1,14 +1,49 @@
 
 
    import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taskati/features/home/home_screan.dart';
 
-class SplashScrean extends StatelessWidget {
+class SplashScrean extends StatefulWidget {
      const SplashScrean({super.key});
 
+  @override
+  State<SplashScrean> createState() => _SplashScreanState();
+}
+
+class _SplashScreanState extends State<SplashScrean> {
+   @override
+   void initState() {
+    super.initState();
+     Future.delayed(Duration(seconds: 5),(){Navigator.push(
+       context,
+       MaterialPageRoute(builder: (context) => HomeScrean()),);});
+  }
      @override
      Widget build(BuildContext context) {
        return Scaffold(
+       body: Center(
+         child: Column( mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             Image.asset("assets/images/tasks.gif"),
+             SizedBox(height:5.h ,),
+             Text("Taskati",
+               style: TextStyle(
+                 fontSize: 18.sp,
+                 fontWeight: FontWeight.bold,
+                 color: Colors.black),),
+             SizedBox(height: 5.h,),
+             Text("Get organized and stay productive with Taskati",
+             style: TextStyle(
+               color:Colors.black54,
+               fontWeight: FontWeight.w600,
+               fontSize: 13.sp
+             ),)
+
+           ],
+         ),
+       )
 
        );
      }
-   }
+}
